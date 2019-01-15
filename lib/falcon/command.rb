f47260053e3ccf61fd2c2d7654e7460074689051
Bastian Schnieders <bastian.schnieders@me.com>
2019-01-15 08:30:56 +0200

    added eventually missing require

--- lib/falcon/command.rb
+++ lib/falcon/command.rb
@@ -17,6 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+require 'logger'
 
 require_relative 'command/serve'
 require_relative 'command/virtual'
